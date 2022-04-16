aa8227b7c963fad87d0dec85c3cdff668016019e
jjty <jye.g96@hotmail.com>
2022-04-16 19:00:30 +1000

    Initial Commit

---
+++ tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tinder
+//
+//  Created by jye on 16/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
