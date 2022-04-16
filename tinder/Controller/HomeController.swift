c85d1085166d307aecdf64c4fdb141fc50eb0ea0
jjty <jye.g96@hotmail.com>
2022-04-16 19:17:39 +1000

    well thats new

---
+++ tinder/Controller/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  tinder
+//
+//  Created by jye on 16/4/2022.
+//
+
+import UIKit
+
+class HomeContoller: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+    
+}
